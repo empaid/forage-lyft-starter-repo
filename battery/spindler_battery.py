7fc00d4136c1520420a1de16c0ec429878fde968
empaid <empaid@empaids-MBP.modem>
2023-10-14 22:21:43 +1100

    Refactor

---
+++ battery/spindler_battery.py
@@ -0,0 +1,10 @@
+from battery.battery import Battery
+from datetime import date
+
+class SpindlerBattery(Battery):
+    def __init__(self, last_service_date:date, current_date:date):
+        self.last_service_date = last_service_date
+        self.current_date = current_date
+    
+    def needs_service(self):
+        return self.last_service_date.replace(years=self.last_service_date.year+2) < self.current_date
---
+++ car_factory.py
@@ -0,0 +1,36 @@
+from engine.capulet_engine import CapuletEngine
+from engine.sternman_engine import SternmanEngine
+from engine.willoughby_engine import WilloughbyEngine
+
+from battery.nubbin_battery import NubbinBattery
+from battery.spindler_battery import SpindlerBattery
+
+from car import Car
+
+from datetime import date
+
+class ClassFactory:
+    def create_calliope(self, current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int):
+        engine = CapuletEngine(last_service_mileage, current_mileage)
+        battery = SpindlerBattery(last_service_date, current_date)
+        return Car(engine, battery)
+    
+    def create_glissade(self, current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int):
+        engine = WilloughbyEngine(last_service_mileage, current_mileage)
+        battery = SpindlerBattery(last_service_date, current_date)
+        return Car(engine, battery)
+    
+    def create_calliope(self, current_date: date, last_service_date: date, warning_light_on: bool):
+        engine = SternmanEngine(warning_light_on)
+        battery = SpindlerBattery(last_service_date, current_date)
+        return Car(engine, battery)
+    
+    def create_calliope(self, current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int):
+        engine = CapuletEngine(last_service_mileage, current_mileage)
+        battery = NubbinBattery(last_service_date, current_date)
+        return Car(engine, battery)
+    
+    def create_calliope(self, current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int):
+        engine = CapuletEngine(last_service_mileage, current_mileage)
+        battery = NubbinBattery(last_service_date, current_date)
+        return Car(engine, battery)
